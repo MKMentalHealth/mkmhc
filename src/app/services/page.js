92514aaad67019d55348c34260dc41be9aebebfd
jennncho <chojenny112@gmail.com>
2024-02-01 13:00:00 -0500

    Services blurb edit1

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -2,7 +2,7 @@ export default function Services() {
     return (
       <h1>SERVICES</h1>
       <p>
-        We offer a wide range of services to meet your needs. We are here to help you reach your goals.
+       range of services to meet your needs. We are here to help you reach your goals.
       </p>
     )
   }
